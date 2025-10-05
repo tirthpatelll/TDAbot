d6be2f14b73b26814aeffc74c8fce42742fe9010
tirthpatelll <158233171+tirthpatelll@users.noreply.github.com>
2025-10-05 01:45:23 -0400

    testing github webhook

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ async def log_action(guild: discord.Guild, text: str):
 async def on_ready():
     logger.info(f"Logged in as {bot.user} (id: {bot.user.id})")
     logger.info("Bot is ready.")
+    logger.info("Testing Github Webhook.")
 
 @bot.event
 async def on_message(message: discord.Message):
